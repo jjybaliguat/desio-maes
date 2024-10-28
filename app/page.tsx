c15c4ede807a54c14e06247ac60e99a2d5ed6010
jjybaliguat <justinejeraldbaliguat@gmail.com>
2024-10-28 10:58:44 +0800

    some changer

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import { LightBulbIcon } from "@heroicons/react/24/solid";
 import { useAuth } from './context/AuthContext';
 import { database, onValue, ref } from './utils/firebase';
-import { set, update } from 'firebase/database';
+import { set } from 'firebase/database';
 
 const Dashboard: React.FC = () => {
   // State to track the active tab and light colors
